8dec50be0b440efce4bb7af3e9a4f1ef095d260d
Skyler Grey <minion@freshlybakedca.ke>
2026-01-02 10:56:17 +0000

    refactor: remove assertions
    Assertions are somewhat of an antipattern as they must be checked on
    every evaluation of your nilla project, whether they are relevant to
    what you're accessing or not. This also means they might take expensive
    actions such as pulling in inputs required for checking.
    
    Assertions are probably fine in personal projects, but we shouldn't be
    using them in core nilla modules. Instead, we can move the assertion to
    checking and throwing on evaluation of the broken system instead.

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -7,7 +7,7 @@ in
     nixos = lib.options.create {
       description = "NixOS systems to create.";
       default.value = { };
-      type = lib.types.attrs.of (lib.types.submodule ({ config }: {
+      type = lib.types.attrs.of (lib.types.submodule ({ config, name }: {
         options = {
           args = lib.options.create {
             description = "Additional arguments to pass to system modules.";
@@ -38,25 +38,19 @@ in
             description = "The created NixOS system.";
             type = lib.types.raw;
             writable = false;
-            default.value = import "${config.pkgs.path}/nixos/lib/eval-config.nix" {
-              pkgs = config.pkgs;
-              lib = config.pkgs.lib;
-              specialArgs = config.args;
-              modules = config.modules;
-              modulesLocation = null;
-            };
+            default.value =
+              if builtins.isNull config.pkgs then
+                builtins.throw "A Nixpkgs instance is required for the NixOS system \"${name}\", but none was provided and \"inputs.nixpkgs\" does not exist."
+              else import "${config.pkgs.path}/nixos/lib/eval-config.nix" {
+                pkgs = config.pkgs;
+                lib = config.pkgs.lib;
+                specialArgs = config.args;
+                modules = config.modules;
+                modulesLocation = null;
+              };
           };
         };
       }));
     };
-  };
-
-  config = {
-    assertions = lib.attrs.mapToList
-      (name: value: {
-        assertion = !(builtins.isNull value.pkgs);
-        message = "A Nixpkgs instance is required for the NixOS system \"${name}\", but none was provided and \"inputs.nixpkgs\" does not exist.";
-      })
-      config.systems.nixos;
   };
 }
